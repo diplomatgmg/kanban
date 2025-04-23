03d114b103164e25ef7691e3a78be5113f2b83ee
Kirill Popov <diplomatgmg@gmail.com>
2025-04-23 18:10:21 +0300

    feat: added tasks endpoint

--- backend/apps/kanban/urls.py
+++ backend/apps/kanban/urls.py
@@ -1,7 +1,8 @@
 from django.urls.conf import path
 
-from apps.kanban.views import TestView
+from apps.kanban.views import TestView, TasksView
 
 urlpatterns = [
-    path("test", TestView.as_view())
+    path("test", TestView.as_view()),
+    path("tasks", TasksView.as_view()),
 ]
--- backend/apps/kanban/views.py
+++ backend/apps/kanban/views.py
@@ -6,3 +6,15 @@ class TestView(APIView):
         return Response({
             "data": "backend is work"
         })
+
+
+
+class TasksView(APIView):
+    def get(self, request, **kwargs):
+        data = [{
+            "id": num,
+            "title": f"Тестовая задача {num}",
+            "description": f"Описание задачи {num}"
+        } for num in range(1, 6)]
+
+        return Response({"data": data})
