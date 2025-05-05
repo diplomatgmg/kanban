9df5fb5cacd41309a73e6561bd618849a5bd8878
lazfnol <lazfnol@gmail.com>
2025-05-05 23:13:12 +0300

    extract BASE_URL to constants.js with dev/prod support

--- frontend/src/api/tasks/getTasks.js
+++ frontend/src/api/tasks/getTasks.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
+import { BASE_URL } from '../../constants.js';
 
 export async function getTasks() {
-    try {
-      const response = await axios.get('http://localhost:8000/api/kanban/tasks');
-      return response.data;
-    } catch (error) {
-      console.error('Ошибка при получении задач:', error);
-      throw error;
-    }
+  try {
+    const response = await axios.get(`${BASE_URL}/api/kanban/tasks`);
+    return response.data;
+  } catch (error) {
+    console.error('Ошибка при получении задач:', error);
+    throw error;
   }
-  
+}
