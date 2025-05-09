1b0e663b7a1cd1383c58f7f05d142349a73613bc
Kirill Popov <diplomatgmg@gmail.com>
2025-05-09 21:25:03 +0300

    fix: imports

--- frontend/src/renderer.js
+++ frontend/src/renderer.js
@@ -1,20 +1,16 @@
-import { getTasks } from './api/tasks/getTasks.js';
+const { getTasks } = require('./api/tasks/getTasks.js');
 
-async function renderTasks() {
+document.addEventListener('DOMContentLoaded', async () => {
     const ul = document.getElementById('tasksList');
     try {
-      const tasks = await getTasks();
-      ul.innerHTML = '';
-      tasks.forEach(task => {
-        const li = document.createElement('li');
-        li.textContent = `${task.id} | ${task.title}`;
-        ul.appendChild(li);
-      });
+        const {data} = await getTasks();
+        ul.innerHTML = '';
+        data.forEach(task => {
+            const li = document.createElement('li');
+            li.textContent = `${task.id} | ${task.title}`;
+            ul.appendChild(li);
+        });
     } catch (error) {
-      ul.innerHTML = '<li>Не удалось загрузить задачи</li>';
+        ul.innerHTML = '<li>Не удалось загрузить задачи</li>';
     }
-  }
-  
-document.addEventListener('DOMContentLoaded', () => {
-    renderTasks();
 });
