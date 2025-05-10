f56ede3971c029f02afde372874982eadb09f953
Kirill Popov <diplomatgmg@gmail.com>
2025-05-10 15:47:24 +0300

    fix: post request
    Took 13 minutes

--- frontend/src/api/tasks/postTasks.js
+++ frontend/src/api/tasks/postTasks.js
@@ -2,9 +2,13 @@ const axios = require('axios');
 const { BASE_URL } = require('../../constants.js');
 
 async function createTask({ name, description }) {
+  if (!name && !description) {
+    throw new Error("name и description обязательны")
+  }
+
   try {
-    const response = await axios.post(`${BASE_URL}/api/kanban/tasks`, {
-      name,
+    const response = await axios.post(`${BASE_URL}/api/kanban/tasks/`, {
+      title: name,
       description,
     });
     return response.data;
